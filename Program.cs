23b36a08fd110a13724bc06e636485e697066d34
Dr. Deckto <fynjy138@gmail.com>
2021-12-14 22:11:25 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ConsoleApp
+{
+    class Program
+    {
+        public static void Main()
+        {
+            int привет = 10;
+
+            привет = 20;
+
+            Console.WriteLine(привет);
+
+            
+        }
+    }
+}
